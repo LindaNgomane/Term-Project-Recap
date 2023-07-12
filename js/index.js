7707f5dbcd32216e7d02453a4fe7e1613f8764de
LindaNgomane <125291929+LindaNgomane@users.noreply.github.com>
2023-07-12 02:35:19 -0700

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,42 @@
+let SandwichOrder = []
+
+displaySandwich = () => {
+
+
+let sandwichName = document.getElementById("name-sandwich").value
+let breadName = document.getElementById("bread-name").textContent
+let breadButton = document.getElementById("bread-button")
+let breadCost = 0
+
+
+if (breadButton.click === true) {
+    breadCost = breadCost + 5
+    breadButton
+}
+
+breadInfo = () => {
+
+
+    let breadButton = document.getElementById("bread-button").addEventListener("click")
+
+
+
+    breadDisableButton = () =>{
+
+
+        let disabledBreadButton = document.getElementById("bread-button")
+        
+        if (breadButton === breadButton ) {
+            breadButton.disabled =true
+        }
+        
+        
+        
+        }
+}
+
+
+console.log(breadButton)
+
+}
+
